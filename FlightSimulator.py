2fa55cc2cb8a6e0447b52a91ee22375fffaf436e
Kavaldrin <turbo3001997@gmail.com>
2019-05-10 00:01:11 +0200

    improvement i guess

--- FlightSimulator.py
+++ FlightSimulator.py
@@ -1,32 +1,118 @@
 from random import gauss
-from time import sleep
+from random import randint
+import logging
 
-class FlightSimulator:
+logging.basicConfig(level=logging.INFO)
 
-	def __init__(self):
-		self.current_angle = 0
+DAMAGE_ANGLE = 60
+CRASH_ANGLE = 90
+MAX_DAMAGE = 10
+SCENARIO_SIZE = 1000
 
-	def fly(self):
-		while True:
-			print("Oh boi, turbulances...")
-			sleep(1)
-			self.generate_turbulance(0,30)
-			print("Current angle: ", self.current_angle)
-			sleep(1)
-			print("Maidai Maidai we need to correct angle... Tilt correction launched")
-			sleep(1)
-			self.correct_angle();
-			print("Thanks God for this device...")
-			print("Current angle (after tilt correction): ", self.current_angle)
-			sleep(1)
 
-	def generate_turbulance(self,mu, sigma):
-		self.current_angle = gauss(mu, sigma)
+class Wind:
+    NO_WIND = 0
+    BREEZE = 1
+    GALE = 2
+    HURRICANE = 3
 
-	def correct_angle(self):
-		if self.current_angle > 0.0:
-			print("Turning right ", abs(int(self.current_angle)), " degrees")
-		else:
-			print("Turning left", abs(int(self.current_angle)), " degrees")
 
-		self.current_angle -= int(self.current_angle)
+class PlaneStatus:
+    OK = 0
+    DAMAGED = 1
+    CRASHED = 2
+
+
+def scenarios_generator():
+    for starting_wind in range(Wind.HURRICANE - Wind.NO_WIND + 1):
+        yield starting_wind
+
+
+class Environment:
+    def __init__(self, wind_level):
+        self.windLevel = wind_level
+        self.planesInSystem = []
+        self.windCounter = 0
+        self.planesStatus = ''
+        self.steps = SCENARIO_SIZE
+
+    def add_plane(self, plane):
+        self.planesInSystem.append(plane)
+
+    def check_planes_status(self):
+
+        crashed_planes = []
+        for plane in self.planesInSystem:
+            if plane.status == PlaneStatus.CRASHED:
+                crashed_planes.append(plane)
+
+        for crashedPlane in crashed_planes:
+            self.planesInSystem.remove(crashedPlane)
+
+    def affect_on_planes(self):
+        for plane in self.planesInSystem:
+            plane.generate_turbulence(self.windLevel)
+            plane.correct_tilt_angle()
+            plane.check_status()
+            self.planesStatus += 'Plane {} tilt {}\n'.format(plane.name, plane.tiltAngle)
+
+    def wind_change(self):
+
+        self.windCounter += 1
+        if self.windCounter >= 10:
+            self.windLevel = randint(Wind.NO_WIND, Wind.HURRICANE)
+            self.windCounter = 0
+
+    def __next__(self):
+
+        self.steps -= 1
+        self.planesStatus = ''
+        if len(self.planesInSystem) > 0 and self.steps > 0:
+            self.check_planes_status()
+            self.affect_on_planes()
+            self.wind_change()
+            return self.planesStatus
+        else:
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+
+class Plane:
+    def __init__(self, name, tilt_correction):
+        logging.info('Creating plane called {} with tilt correction {}'.format(name, tilt_correction))
+        self.tiltAngle = 0
+        self.name = name
+        self.tiltCorrection = tilt_correction
+        self.status = PlaneStatus.OK
+        self.damage_counter = 0
+
+    def generate_turbulence(self, wind_level):
+        self.tiltAngle += gauss(0.75, 1.0 * wind_level)
+        logging.info('Tilt after turbulence {}'.format(self.tiltAngle))
+
+    def correct_tilt_angle(self):
+        tilt_correction = min(abs(self.tiltCorrection), abs(self.tiltAngle))
+        logging.info('Correction of tilt {} degrees'.format(tilt_correction))
+
+        if self.tiltAngle > 0:
+            logging.info('Turning left for {} degrees'.format(tilt_correction))
+            self.tiltAngle -= tilt_correction
+        else:
+            logging.info('Turning right for {} degrees'.format(tilt_correction))
+            self.tiltAngle += tilt_correction
+
+    def check_status(self):
+        if self.tiltAngle > DAMAGE_ANGLE or self.tiltAngle < -DAMAGE_ANGLE:
+            logging.warning('Plane is getting DAMAGE!')
+            self.status = PlaneStatus.DAMAGED  # can be overwritten :)
+            self.damage_counter += 1
+            if self.damage_counter > MAX_DAMAGE:
+                logging.critical('Plane has crashed :(')
+                self.status = PlaneStatus.CRASHED
+                return None  # just to exit function faster
+
+        elif self.tiltAngle > CRASH_ANGLE or self.tiltAngle < -CRASH_ANGLE:
+            logging.critical('Plane has crashed :(')  # same line, but to avoid changing logic
+            self.status = PlaneStatus.CRASHED
--- task.py
+++ task.py
@@ -1,29 +1,37 @@
 #
-#Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction. 
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations"). 
+# Flight simulator.
+# Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth).
+# The program should print out current orientation, and applied tilt correction.
+# The program should run in infinite loop, until user breaks the loop.
+# Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations").
 
-#With every simulation step the orentation should be corrected, applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
+# With every simulation step the orentation should be corrected, applied and printed out.
+# Try to expand your implementation as best as you can.
+# Think of as many features as you can, and try implementing them.
 #
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
+# Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
+# Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
+# The goal of this task is for you to SHOW YOUR BEST python programming skills.
+# Impress everyone with your skills, show off with your code.
 #
-#When you are done upload this code to your github repository. 
+# When you are done upload this code to your github repository.
 #
-#Delete these comments before commit!
-#Good luck.
+# Delete these comments before commit!
+# Good luck.
 
 
-from FlightSimulator import FlightSimulator
+from FlightSimulator import Plane
+from FlightSimulator import Environment
+from FlightSimulator import scenarios_generator
+from time import sleep
 
 if __name__ == "__main__":
-	simulator = FlightSimulator()
-	simulator.fly()
-	#so here should be infinite loop with prints
-	#and generator should generate turbulance
+    for wind_scenario in scenarios_generator():
+        environment = Environment(wind_scenario)
+        environment.add_plane(Plane("F2-1", 1))
+        environment.add_plane(Plane("F15", 1.5))
+        environment.add_plane(Plane("Boeing 777", 0.5))
+
+        for simulation_step in environment:
+            sleep(1)
+            print(simulation_step)
